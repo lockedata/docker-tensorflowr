4a40d0ccf6e08ae24d722cf98d0fded4b38771f9
Steph <Steph@itsalocke.com>
2017-10-04 17:33:55 +0100

    No longer req'd

--- sampleSQL.r
+++
@@ -1,8 +0,0 @@
-library(DBI)
-library(odbc)
-dbConn<-dbConnect(odbc(),
-          driver="SQL Server",
-          server="mhknbn2kdz.database.windows.net",
-          database="AdventureWorks2012",
-          uid="sqlfamily",
-          pwd="sqlf@m1ly")
